0c11f31af541f6e5c010321b71b91b47b1b3a64f
Evrid <107913134+Evrid@users.noreply.github.com>
2023-08-27 11:14:16 +0200

    Add files via upload

--- arbsavejob.py
+++ arbsavejob.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import os
 from selenium import webdriver
-import time  # Import the time module at the top of your script
+import time
+import datetime  # Don't forget to import the datetime module
 
 def scrape_and_save_job_info(url, excel_filename='+Applied jobs.xlsx'):
     # Send a request to the URL
@@ -22,22 +23,28 @@ def scrape_and_save_job_info(url, excel_filename='+Applied jobs.xlsx'):
         soup = BeautifulSoup(html_content, 'html.parser')
         
         # Extract information
-        job_name = soup.find('h1').text if soup.find('h1') else "N/A"   #job name
-        company_name = soup.find('h2').text if soup.find('h2') else "N/A"  #Company Name
-        job_type = soup.find('h3').text if soup.find('h3') else "N/A"  #job_type
+        job_name = soup.find('h1').text if soup.find('h1') else "N/A"
+        company_name = soup.find('h2').text if soup.find('h2') else "N/A"
+        job_type = soup.find('h3').text if soup.find('h3') else "N/A"
         
         # Finding second h3 for location
         h3_tags = soup.find_all('h3')
         location = h3_tags[1].text if len(h3_tags) > 1 else "N/A"
         
         # Store the information
-        new_row = {'Job Name': job_name, 'Company Name': company_name, 'Job Type': job_type, 'Location': location}
+        new_row = {
+            'Job Name': job_name,
+            'Company Name': company_name,
+            'Job Type': job_type,
+            'Location': location,
+            'AppliedDate': datetime.datetime.now().date().strftime('%Y-%m-%d')
+        }
         
         # Check if the Excel file already exists
         if os.path.exists(excel_filename):
             df = pd.read_excel(excel_filename)
         else:
-            df = pd.DataFrame(columns=['Job Name', 'Company Name', 'Job Type', 'Location'])
+            df = pd.DataFrame(columns=['Job Name', 'Company Name', 'Job Type', 'Location', 'AppliedDate'])
         
         # Append the new job to the DataFrame
         df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
